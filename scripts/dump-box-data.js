17af91a79a1fc8c9602a3d718a019e628f399f2b
Martin <mr.mmaurer+work@gmail.com>
2019-01-15 00:06:48 +0100

    Added dumper, mocks and updated get script

--- scripts/dump-box-data.js
+++ scripts/dump-box-data.js
@@ -1,20 +1,22 @@
 import fetch from 'node-fetch';
+import uid from 'uid';
+import fs from 'fs-extra';
 
 const url = 'http://fritz.box/cgi-bin/system_status';
 
 fetch(url, { compress: false })
   .then((response) => {
     if (response.ok) {
-      return response.text();
+      const randomHash = uid(5);
+      const newFile = fs.createWriteStream(`./mocks/box-${randomHash}.txt`);
+      const content = response.body;
+
+      content.pipe(newFile);
+
+      return;
     }
 
     throw Error(`Error HTTP-${response.status}`);
   }).catch((error) => {
     console.log(error);
-  })
-  .then((response) => {
-    console.log(response);
-
-    return response;
   });
-  
