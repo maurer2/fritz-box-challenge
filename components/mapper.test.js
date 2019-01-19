440748900e5a9e11032ef269efce8057590db61d
Martin <mr.mmaurer+work@gmail.com>
2019-01-19 23:49:27 +0100

    Added jest

---
+++ components/mapper.test.js
@@ -0,0 +1,27 @@
+import { fieldsMappings, mapValueToField } from './mapper';
+
+const testData = [
+  'FRITZ!Box 7590 (UI)',
+  'B',
+  '041711–000',
+  '121',
+  '533176–734744',
+  '147902',
+  '840604',
+  '28179',
+  '1und1',
+];
+
+test('fieldsMapping is not empty ', () => {
+  const mappings = fieldsMappings;
+
+  expect(mappings.length).toBeGreaterThan(0);
+});
+
+test('mapValueToField returns proper object ', () => {
+  const index = 0;
+  const key = fieldsMappings[index];
+  const value = testData[index];
+
+  expect(mapValueToField(value, index)).toEqual({ [key]: value });
+});
