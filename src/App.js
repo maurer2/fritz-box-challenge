8f3d7d1cbc13b486539a70284b2d35463b51b593
Martin <mr.mmaurer+work@gmail.com>
2019-02-28 00:56:42 +0100

    Minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components/macro';
 import { CSSTransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
@@ -10,71 +10,70 @@ import StatusBar from './components/StatusBar';
 const AppWrapper = styled.div`
   display: flex;
   margin: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  overflow-y: hidden;
-  flex-direction: column;
+  padding: 0;
+  width: 100%; 
+  height: 100%;
+  justify-content: center;
+  align-items: center;
   background: black;  
 `;
 
 const MainWrapper = styled.main`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
+  display: block;
 
-  .example-appear {
+  .fade-appear {
     opacity: 0.1;
-    // background: red;
+    background: red;
   }
 
-  .example-enter {
+  .fade-enter {
     opacity: 0.1;
-    // background: green;
+    background: green;
   }
 
-  .example-appear.example-appear-active {
+  .fade-appear.fade-appear-active {
     opacity: 1;
-    // transition: opacity .5s ease-in;
+    transition: opacity 500ms ease-in;
   }
 
-  .example-enter.example-enter-active {
+  .fade-enter.fade-enter-active {
     opacity: 1;
-    // transition: opacity .5s ease-in;
+    transition: opacity 500ms ease-in;
   }
 `;
 
-class App extends Component {
-  render() {
-    const { dateLong, dateProsa, boxInformation, isUpdating, showFullNumber } = this.props;
+const App = (props) => {
+  const { dateLong,
+    dateProsa,
+    boxInformation,
+    isUpdating,
+    showFullNumber,
+    handleClickEvent } = props;
 
-    const dateLongComponent = <Slide text={ dateLong } title="Production date" key={ 1 } />;
-    const dateProsaComponent = <Slide text={ dateProsa } title="Age" key={ 2 } />;
+  const dateLongComponent = <Slide text={ dateLong } title="Production date" key={ 1 } />;
+  const dateProsaComponent = <Slide text={ dateProsa } title="Age" key={ 2 } />;
 
-    return (
-      <AppWrapper onClick={ this.props.handleClickEvent }>
-        <UpdateBar isUpdating={ isUpdating } />
-        <MainWrapper>
-          { !isUpdating && (
-            <CSSTransitionGroup
-              component={ React.Fragment }
-              transitionName="example"
-              transitionAppear={ true }
-              transitionAppearTimeout={ 500 }
-              transitionLeave={ false }
-              transitionEnterTimeout={ 500 }
-            >
-              { showFullNumber ? dateLongComponent : dateProsaComponent }
-            </CSSTransitionGroup>
-          )}
-        </MainWrapper>
-        <StatusBar isUpdating={ isUpdating } list={ boxInformation } />
-      </AppWrapper>
-    );
-  }
-}
+  return (
+    <AppWrapper onClick={ handleClickEvent }>
+      <UpdateBar isUpdating={ isUpdating } />
+      <MainWrapper>
+        { !isUpdating && (
+          <CSSTransitionGroup
+            component={ React.Fragment }
+            transitionName="fade"
+            transitionAppear={ true }
+            transitionAppearTimeout={ 500 }
+            transitionLeave={ false }
+            transitionEnterTimeout={ 500 }
+          >
+            { showFullNumber ? dateLongComponent : dateProsaComponent }
+          </CSSTransitionGroup>
+        )}
+      </MainWrapper>
+      <StatusBar isUpdating={ isUpdating } list={ boxInformation } />
+    </AppWrapper>
+  );
+};
 
 App.propTypes = {
   dateLong: PropTypes.string,
