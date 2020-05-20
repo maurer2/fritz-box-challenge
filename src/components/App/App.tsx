4a4ce0eb31c2ce8e70d234b2ea9cce803ba8612e
Martin <mr.mmaurer+work@gmail.com>
2020-05-20 23:53:06 +0200

    Got context running in updatebar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useContext } from 'react';
 import { Normalize } from 'styled-normalize';
 
 import { MainContent } from '../MainContent';
@@ -7,71 +7,28 @@ import { NavBar } from '../NavBar';
 import { Slide } from '../Slide';
 import { Theme } from '../Theme';
 import { UpdateBar } from '../UpdateBar';
+import { DataProvider, BoxDataContext } from '../DataProvider';
 
 import * as Types from './App.types';
 import * as Styles from './App.styles';
 
-const App: React.FC<Types.AppProps> = ({
-  boxData,
-  isUpdating,
-  isValid,
-  componentsToShow,
-}): JSX.Element => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const oldIndex = useRef(0);
+const App: React.FC = (): JSX.Element => {
+  const boxData = {};
+  const isUpdating = true;
+  const isValid = true;
 
-  const title = componentsToShow[currentIndex] || '';
-  const text = boxData[title] || '';
-
-  function handleClick(): void {
-    const lastIndex = componentsToShow.length - 1;
-    const newCurrentIndex = currentIndex < lastIndex ? currentIndex + 1 : 0;
-
-    oldIndex.current = currentIndex;
-    setCurrentIndex(newCurrentIndex);
-  }
-
-  function handleNavigation(index: number): void {
-    oldIndex.current = currentIndex;
-    setCurrentIndex(index);
-  }
-
-  const showContent = !isUpdating && isValid;
+  const showContent = false;
 
   return (
-    <Theme>
-      <Normalize />
-      <Styles.AppWrapper>
-        <UpdateBar isUpdating={isUpdating} isValid={isValid} />
-        {showContent && (
-          <MainContent
-            handleClick={handleClick}
-            currentIndex={currentIndex}
-            oldIndex={oldIndex.current}
-          >
-            <Slide title={title} text={text} key={title} />
-          </MainContent>
-        )}
-        <NavBar
-          componentsToShow={componentsToShow}
-          currentIndex={currentIndex}
-          handleNavigation={handleNavigation}
-          isUpdating={!showContent}
-        />
-      </Styles.AppWrapper>
-    </Theme>
+    <DataProvider>
+      <Theme>
+        <Normalize />
+        <Styles.AppWrapper>
+          <UpdateBar isUpdating={isUpdating} isValid={isValid} />
+        </Styles.AppWrapper>
+      </Theme>
+    </DataProvider>
   );
-};
-
-const {
-  bool, objectOf, string, arrayOf,
-} = PropTypes;
-
-App.propTypes = {
-  boxData: objectOf(string).isRequired,
-  isUpdating: bool.isRequired,
-  isValid: bool.isRequired,
-  componentsToShow: arrayOf(string).isRequired,
 };
 
 export { App };
--- src/components/UpdateBar/UpdateBar.tsx
+++ src/components/UpdateBar/UpdateBar.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+
+import { BoxDataContext } from '../DataProvider';
 
 import * as Styles from './UpdateBar.styles';
 import * as Types from './UpdateBar.types';
 
-const UpdateBar: React.FC<Types.UpdateBarProps> = ({ isUpdating, isValid }): JSX.Element => (
-  <Styles.UpdateBar isUpdating={isUpdating}>
-    {isUpdating ? (
-      <Styles.Text>Updating!</Styles.Text>
-    ) : (
-      <Styles.Text>
-        {!isValid && <>Error!</>}
-        {!!isValid && <>Data loaded!</>}
-      </Styles.Text>
-    )}
-  </Styles.UpdateBar>
-);
+const UpdateBar: React.FC<Types.UpdateBarProps> = (): JSX.Element => {
+  const state = React.useContext(BoxDataContext);
+  console.log(state);
+
+  const isUpdating = true;
+  const isValid = true;
+
+  return (
+    <Styles.UpdateBar isUpdating={isUpdating}>
+      {isUpdating ? (
+        <Styles.Text>Updating!</Styles.Text>
+      ) : (
+        <Styles.Text>
+          {!isValid && <>Error!</>}
+          {!!isValid && <>Data loaded!</>}
+        </Styles.Text>
+      )}
+    </Styles.UpdateBar>
+  );
+};
 
 const { bool } = PropTypes;
 
