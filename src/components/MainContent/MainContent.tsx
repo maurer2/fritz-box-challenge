1a14240078bd4951af26c2436ef49dd9d212c133
Martin <mr.mmaurer+work@gmail.com>
2020-05-27 23:31:41 +0200

    Fixed slideLeft condition

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
 
@@ -9,13 +9,13 @@ import * as Styles from './MainContent.styles';
 
 const MainContent: React.FC<{}> = (): JSX.Element => {
   const state = React.useContext(BoxDataContext);
-  const oldIndex = useRef(0 as number);
 
   const componentsToShow = 'componentsToShow' in state ? state.componentsToShow : [];
   const boxData = 'boxData' in state ? state.boxData : {};
   const currentIndex = 'currentIndex' in state ? state.currentIndex : 0;
+  const prevIndex = 'prevIndex' in state ? state.prevIndex : 0;
 
-  const slideInFromRight = currentIndex > oldIndex.current;
+  const slideInFromRight = currentIndex > prevIndex;
   const title = componentsToShow[currentIndex] || '';
   const text = (boxData as any)[title] || '';
 
@@ -23,7 +23,6 @@ const MainContent: React.FC<{}> = (): JSX.Element => {
     const indexOfLastEntry = componentsToShow.length - 1;
     const newCurrentIndex = currentIndex < indexOfLastEntry ? currentIndex + 1 : 0;
 
-    oldIndex.current = currentIndex;
     state.updateCurrentIndex(newCurrentIndex);
   }
 
