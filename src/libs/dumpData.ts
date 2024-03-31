fb280b664320af2f3aab5f027ce19b1a335790f1
Martin <mr.mmaurer+work@gmail.com>
2024-03-31 22:41:31 +0200

    Migrated to pnpm, removed uid dependency and updated other dependencies

--- src/libs/dumpData.ts
+++ src/libs/dumpData.ts
@@ -1,8 +1,6 @@
-// import { uid } from 'uid';
 import fs from 'fs-extra';
 
 const dumpData = (data: unknown) => new Promise((resolve, reject) => {
-  // const newFile = fs.createWriteStream(`${__dirname}/../mocks/box-${uid(5)}.txt`);
   const newFile = fs.createWriteStream(`${__dirname}/../../public/mock-data.txt`);
 
   newFile.on('error', () => {
