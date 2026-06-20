8a74edf5c8ac6def2ed24f2cdef44b49ebff390e
Martin <mr.mmaurer@gmail.com>
2026-06-20 14:25:54 +0200

    Added example for callback ref and compiler issue

--- src/hooks/CompilerTest/CompilerTest.tsx
+++ src/hooks/CompilerTest/CompilerTest.tsx
@@ -1,7 +1,8 @@
 // oxlint-disable no-unused-vars
-import { useEffect, useRef, useState } from 'react';
+// oxlint-disable react/no-multi-comp, react/only-export-components
+import { useEffect, useRef, useState, type Ref } from 'react';
 
-const useAccessRefsDuringRender = (value: unknown) => {
+const useAccessRefDuringRender = (value: unknown) => {
   const ref = useRef<unknown>(null);
 
   useEffect(() => {
@@ -17,4 +18,33 @@ const useIncorrectDependencyArray = () => {
   useEffect(() => {
     console.log(name);
   }, []);
+};
+
+const ChildComponent = ({ ref }: { ref?: Ref<HTMLDivElement> }) => <div ref={ref}>Test</div>;
+const AccessCallbackRefDuringRender = () => {
+  const [offset, setOffset] = useState<number | null>(null);
+
+  const callbackRefs = {
+    test: (element: HTMLElement | null) => {
+      if (!element) {
+        return;
+      }
+
+      setOffset(element.getBoundingClientRect().top);
+    },
+    test2: (element: HTMLElement | null) => element?.focus(),
+  };
+  // oxlint-disable-next-line unicorn/consistent-function-scoping
+  const callbackRef = (element: HTMLElement | null) => element?.focus();
+
+  console.log(offset);
+
+  return (
+    <>
+      {/* Error - Cannot access refs during render */}
+      <ChildComponent ref={callbackRefs.test} />
+      {/* No error */}
+      <ChildComponent ref={callbackRef} />
+    </>
+  );
 };
