7d4c9ac2f075d99a69fb2039e005849a13863f13
Martin <mr.mmaurer@gmail.com>
2026-06-27 01:34:01 +0200

    Added view transitions for section and refactored ts

--- experiments/bar-transition-via-view-transitions/src/main.ts
+++ experiments/bar-transition-via-view-transitions/src/main.ts
@@ -4,27 +4,46 @@ const app = document.querySelector<HTMLDivElement>('#app');
 if (!app) {
   throw new Error('Root element missing');
 }
-
 const linkList = app.querySelector<HTMLUListElement>('.links');
 if (!linkList) {
   throw new Error('Nav links missing');
 }
+const links = new Set(linkList.querySelectorAll<HTMLAnchorElement>('.link'));
+const sections = new Set(app.querySelectorAll<HTMLElement>('.section'));
 
-const links = [...linkList.querySelectorAll<HTMLAnchorElement>('.link')];
+const setActiveLink = (activeLink: HTMLAnchorElement): void => {
+  const nonActiveLinks = links.difference(new Set([activeLink]));
 
-linkList.addEventListener('click', (event) => {
-  const newActiveLink = event.target;
-  if (!newActiveLink) {
+  nonActiveLinks.forEach((link) => link.removeAttribute('aria-current'));
+  activeLink.setAttribute('aria-current', 'page');
+};
+
+const setActiveSection = (targetSectionId: string): void => {
+  const activeSection = app.querySelector<HTMLElement>(targetSectionId);
+  if (!activeSection) {
     return;
   }
 
+  const nonActiveSections = sections.difference(new Set([activeSection]));
+
+  nonActiveSections.forEach((section) => (section.hidden = true));
+  activeSection.hidden = false;
+};
+
+linkList.addEventListener('click', (event) => {
+  if (!(event.target instanceof Element)) {
+    return;
+  }
+
+  const newActiveLink = event.target.closest<HTMLAnchorElement>('.link');
+  const newTargetId = newActiveLink?.getAttribute('href');
+
+  if (!newActiveLink || !newTargetId) {
+    throw new Error('New link missing or incorrect link target');
+  }
+
   document.startViewTransition(() => {
-    for (const link of links) {
-      if (link === newActiveLink) {
-        link.setAttribute('aria-current', 'page');
-      } else {
-        link.removeAttribute('aria-current');
-      }
-    }
+    setActiveLink(newActiveLink);
+    setActiveSection(newTargetId);
   });
 });
